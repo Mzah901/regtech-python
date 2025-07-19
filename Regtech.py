38f8a43aee8f0191bb7d3bba1fdbf5b3a951ac6f
Oxieoxie!! <mac@MacBook-Pro-2.local>
2025-07-19 08:18:15 +0700

    Initial commit: greeting script

---
+++ Regtech.py
@@ -0,0 +1,5 @@
+name = input("What is your name?")
+field = input("What's your field of interest? (e.g., finance, healthcare, etc.)?")
+
+print(f"Hello, Mzah!")
+print(f"You're on your way to becoming a RegTech engineer in the Legal sector 🚀")
